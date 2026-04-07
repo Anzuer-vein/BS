8f2fad6000fdacacc9c25af50504c21047be7c7f
Anzuer-vein <cshun9614@gmail.com>
2026-04-07 16:45:48 +0800

    提交文件

---
+++ animal-server/src/main/java/com/example/service/IArticleService.java
@@ -0,0 +1,15 @@
+package com.example.service;
+
+import com.example.entity.Article;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author
+ */
+public interface IArticleService extends IService<Article> {
+
+}
